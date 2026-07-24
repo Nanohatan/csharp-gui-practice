132dd599b21796ec390016b2eaae6d21a599cdef
Nanohatan <koaramarch0101@gmail.com>
2026-07-24 21:43:33 +0900

    プロジェクトをリセット [skip ci]

--- CounterApp/ViewModels/ProductListViewModel.cs
+++
@@ -1,9 +0,0 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace CounterApp.ViewModels;
-
-public partial class ProductListViewModel : ObservableObject
-{
-    [ObservableProperty]
-    private string _title = "商品";
-}
