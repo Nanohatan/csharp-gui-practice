57f04250e5651f2405a347e8f10dcbb532214d1f
Nanohatan <koaramarch0101@gmail.com>
2026-08-02 12:44:27 +0900

    refactor: formatting [skip ci]

--- OmamagotoApp/Features/Home/HomePage.cs
+++ OmamagotoApp/Features/Home/HomePage.cs
@@ -4,32 +4,32 @@ namespace OmamagotoApp.Features.Home;
 
 public partial class HomePage : ContentPage
 {
-	int _count = 0;
+    int _count = 0;
 
-	public HomePage()
-	{
-	}
+    public HomePage()
+    {
+    }
 
-	public HomePage(HomeViewModel viewModel)
-	{
-		InitializeComponent();
-		BindingContext = viewModel;
-	}
+    public HomePage(HomeViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
 
-	private void OnCounterClicked(object? sender, EventArgs e)
-	{
-		_count++;
+    private void OnCounterClicked(object? sender, EventArgs e)
+    {
+        _count++;
 
-		if (_count == 1)
-			CounterBtn.Text = $"Clicked {_count} time";
-		else
-			CounterBtn.Text = $"Clicked {_count} times";
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        if (_count == 1)
+            CounterBtn.Text = $"Clicked {_count} time";
+        else
+            CounterBtn.Text = $"Clicked {_count} times";
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 
-	private async void OnCreateProductClicked(object? sender, EventArgs e)
-	{
-		await Shell.Current.GoToAsync(nameof(ProductEditPage));
-	}
+    private async void OnCreateProductClicked(object? sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(ProductEditPage));
+    }
 
 }
