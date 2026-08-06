8c5b1eba8e0f62719d082be989106cc299dd412f
Nanohatan <koaramarch0101@gmail.com>
2026-08-07 06:20:29 +0900

    refactor: add comments

--- OmamagotoApp/Services/Database/IDatabaseService.cs
+++ OmamagotoApp/Services/Database/IDatabaseService.cs
@@ -2,7 +2,14 @@ using SQLite;
 
 namespace OmamagotoApp.Services.Database;
 
+/// <summary>
+/// DBの作成、接続を提供するサービス。
+/// </summary>
 public interface IDatabaseService
 {
+    /// <summary>
+    /// DBへ接続する。
+    /// </summary>
+    /// <returns></returns>
     Task<SQLiteAsyncConnection> GetConnectionAsync();
 }
--- OmamagotoApp/Services/Dialogs/IDialogService.cs
+++ OmamagotoApp/Services/Dialogs/IDialogService.cs
@@ -1,19 +1,46 @@
 namespace OmamagotoApp.Services.Dialogs;
 
+/// <summary>
+/// ダイアログの表示を提供するサービス。
+/// </summary>
 public interface IDialogService
 {
+    /// <summary>
+    /// 情報ダイアログを表示する。
+    /// </summary>
+    /// <param name="message">メッセージ。</param>
+    /// <param name="title">ダイアログのタイトル。</param>
+    /// <returns></returns>
     Task ShowInformationAsync(
         string message,
         string title = "情報");
 
+    /// <summary>
+    /// 警告ダイアログを表示する。
+    /// </summary>
+    /// <param name="message">メッセージ本文。</param>
+    /// <param name="title">ダイアログのタイトル。</param>
+    /// <returns></returns>
     Task ShowWarningAsync(
         string message,
         string title = "警告");
 
+    /// <summary>
+    /// エラーダイアログを表示する。
+    /// </summary>
+    /// <param name="message">メッセージ本文。</param>
+    /// <param name="title">ダイアログのタイトル。</param>
+    /// <returns></returns>
     Task ShowErrorAsync(
         string message,
         string title = "エラー");
 
+    /// <summary>
+    /// 確認ダイアログを表示する。
+    /// </summary>
+    /// <param name="message">メッセージ本文。</param>
+    /// <param name="title">ダイアログのタイトル。</param>
+    /// <returns></returns>
     Task<bool> ConfirmAsync(
         string message,
         string title = "確認");
